df1b3b563e85621eec2b8884328644eb06aa1181
Aneeza <amali78696@gmail.com>
2022-09-14 11:29:44 +0200

    user api

---
+++ middleware/password.js
@@ -0,0 +1,17 @@
+const passwordSchema = require("../models/password");
+
+//checking if user password is ok with our password model.
+module.exports = (req, res, next) => {
+  if (!passwordSchema.validate(req.body.password)) {
+    res.writeHead(
+      400,
+      "Le mot de passe doit comprendre 8 caractères dont un chiffre, sans espaces",
+      {
+        "content-type": "application/json",
+      }
+    );
+    res.end("Le format du mot de passe est incorrect.");
+  } else {
+    next();
+  }
+};
